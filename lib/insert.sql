c4f3a4bbbc82643b0722d9b5ea68869fb78dda81
Yeeun Jung <rjyeeun@gmail.com>
2023-02-01 23:49:30 -0500

    commit tree 559a7a2ce32b60a163e632fa74dd5118417a55b2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', '3', 'M', 'Brown', 'high', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', '5', 'F', 'Black', 'low', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', '2', 'M', 'Brown', 'high', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', '7', 'M', 'Brown', 'low', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', '1', 'F', 'Brown', 'low', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', '9', 'M', 'Brown', 'high', 'False');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', '4', 'F', 'Black', 'low', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, '2', 'M', 'Brown', 'high', 'False');
